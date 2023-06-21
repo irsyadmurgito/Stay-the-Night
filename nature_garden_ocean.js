78bebc0d6e10c1102d445d65636f7df715af6292
irsyadmurgito <136702794+irsyadmurgito@users.noreply.github.com>
2023-06-21 13:07:02 +0700

    Optimize database schema

---
+++ nature_garden_ocean.js
@@ -0,0 +1,92 @@
+by Zedd
+
+// Write a function to play 'Stay the Night'
+function playStayTheNight(){
+    const Audio = new Audio("StayTheNight.mp3");
+    Audio.play();
+};
+
+// Write a function to get the lyrics
+function getLyrics(){
+    console.log("She's so beautiful, and I tell her every day");
+    console.log("Yeah, I know that I'm bound to her in every way");
+    console.log("I can barely breathe when she is around");
+    console.log("Just one touch, and I lose myself within the sound");
+    console.log("");
+    console.log("[Chorus]");
+    console.log("So don't worry if it's not enough");
+    console.log("I know I'm never giving up");
+    console.log("I just gotta do what I can do");
+    console.log("Stay the night, stay the night, yeah");
+    console.log("");
+    console.log("When the morning comes around");
+    console.log("I'm feeling safe and sound");
+    console.log("There's no need to say goodbye");
+    console.log("Until the sky turns blue");
+    console.log("The stars that fill the air");
+    console.log("We'll stay right here, until the end of time");
+    console.log("[Chorus]");
+    console.log("So don't worry if it's not enough");
+    console.log("I know I'm never giving up");
+    console.log("I just gotta do what I can do");
+    console.log("Stay the night, stay the night, yeah");
+    console.log("ohhh oooh");
+    console.log("ohhh oooh");
+    console.log("");
+    console.log("I'm living it out on the edge of the night");
+    console.log("My heart and soul make this feeling so right");
+    console.log("It's like an endless dream, that I never wanna end");
+    console.log("So don't ever let me go til the sun comes up again");
+    console.log("");
+    console.log("[Chorus]");
+    console.log("So don't worry if it's not enough");
+    console.log("I know I'm never giving up");
+    console.log("I just gotta do what I can do");
+    console.log("Stay the night, stay the night, yeah");
+    console.log("ohhh oooh");
+    console.log("ohhh oooh");
+    console.log("ohhh oooh");
+    console.log("ohhh oooh");
+    console.log("Stay the night, stay the night, yeah");
+
+}
+
+// Write a function that gets the singer's name
+function getSingers(){
+    console.log("Zedd")
+}
+
+// Write a function to get the title of the song
+function getTitle(){
+    console.log("Stay the Night");
+}
+
+// Write a function to get the genre
+function getGenre(){
+    console.log("EDM");
+}
+
+// Write a function to get the release date
+function getReleaseDate(){
+    console.log("13 August 2013");
+}
+
+// Write a function to get the song length
+function getLength(){
+    console.log("3:35");
+}
+
+// Write a function to get the record label
+function getRecordLabel(){
+    console.log("Interscope Records");
+}
+
+// Write a function to get the album
+function getAlbum(){
+    console.log("Clarity")
+}
+
+// Write a function to get the producers
+function getProducers(){
+    console.log("Zedd & Kevin drew");
+}
